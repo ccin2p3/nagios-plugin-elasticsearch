08700965a574e0c936036f4dd242ee80b4738808
Peter Adam <p.adam@cygnusnetworks.de>
2014-10-01 11:58:19 +0200

    Updated requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from setuptools import setup
 
 setup(
     name="nagios-plugin-elasticsearch",
     description="An ElasticSearch availability and performance monitoring plugin for Nagios.",
     version="1.0.2",
-    packages=find_packages(),
-    url="https://github.com/anchor/nagios-plugin-elasticsearch",
-    maintainer="Sharif Olorin",
-    maintainer_email="sio@tesser.org",
-    author="Saj Goonatilleke",
-    author_email="sg@redu.cx",
+    packages=[],
+    url="https://github.com/CygnusNetworks/nagios-plugin-elasticsearch",
+    author="Peter Adam",
+    author_email="info@cygusnetworks.de",
     scripts=["check_elasticsearch"],
     license="MIT",
-    install_requires=[str(req.req) for req in
-                          parse_requirements("requirements.txt")],
-    include_package_data=True
+    install_requires=['nagiosplugin'],
 )
